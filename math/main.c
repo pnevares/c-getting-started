77a136151b3471304d636990aac20961abc9c412
Pablo Nevares <pablo@nevares.com>
2018-08-15 22:24:21 -0700

    math functions

---
+++ math/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+  printf("2^3: %f\n", pow(2, 3));
+  printf("√81: %f\n", sqrt(81));
+  printf("|-100|: %d\n", abs(-100));
+
+  return 0;
+}
