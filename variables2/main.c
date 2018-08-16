15de076a766ba089a18e3a6769baea4324aa5413
Pablo Nevares <pablo@nevares.com>
2018-08-15 22:13:03 -0700

    more variables

---
+++ variables2/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+  int year = 1997;
+  double balance = 5500.22;
+  char letter = 'Z';
+  char state[] = "California";
+
+  printf("Integer: %d\n", year);
+  printf("Double: %f\n", balance);
+  printf("Character: %c\n", letter);
+  printf("String: %s\n", state);
+
+  return 0;
+}
