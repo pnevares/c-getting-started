39236762ad7dbcb2a6e02350f0c1430962481048
Pablo Nevares <pablo@nevares.com>
2018-08-18 22:14:18 -0700

    updated number game with rand

--- guessing/main.c
+++ guessing/main.c
@@ -1,17 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <time.h>
+
+int takeGuess(int);
 
 int main() {
-  int number = rand() % 10;
-  int guess;
+  int range = 20;
+  int guess = takeGuess(range);
+  int oldDifference = range + 1;
 
-  do {
-    printf("Try to guess the random number! (0-9): ");
-    scanf("%d", &guess);
-  } while(guess != number);
+  srand(time(NULL));
+  int number = rand() % range;
 
-  printf("You got it!\n");
+  while (guess != number) {
+    int newDifference = abs(number - guess);
+    if (newDifference > oldDifference) {
+      printf("Getting colder...\n");
+    } else {
+      printf("Getting warmer...\n");
+    }
+    oldDifference = newDifference;
+
+    guess = takeGuess(range);
+  }
+
+  printf("You guessed correctly!\n");
 
   return 0;
+}
+
+int takeGuess(int range) {
+  int guess;
+  printf("Try to guess the random number! (0-%d): ", range - 1);
+  scanf("%d", &guess);
+  return guess;
 }
